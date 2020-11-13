9cc407754a22a55d53765d9e7f53036ff9e35588
Daniel A. Alvarado <danielalvarado019@gmail.com>
2020-11-13 14:42:43 -0600

    Generacion y Manual Listo

--- P3Lab5_DanielAlvarado.cpp
+++ P3Lab5_DanielAlvarado.cpp
@@ -12,14 +12,260 @@
  */
 
 #include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <ctime>
 
 using namespace std;
+
+//Prototipos
+int menu();
+int** genMatrizManual(int, int);
+int** genMatrizAuto(int, int);
+void freeHeapVector();
+void imprimirMatriz();
 
 /*
  * 
  */
+
+vector<int**> matricesVector;
+vector<int> filasMatrices;
+vector<int> columnasMatrices;
+
+
 int main(int argc, char** argv) {
+
+//    vector<int**> matricesVector;
+//    vector<int> filasMatrices;
+//    vector<int> columnasMatrices;
+
+    int opcion = 0;
+
+    while (opcion != 4) {
+
+        switch ((opcion = menu())) {
+
+            case 1:
+            {
+
+                cout << "Ingreso Manual de matriz" << endl << endl;
+
+                int filas = 0;
+                int columnas = 0;
+
+                cout << "Ingrese las filas de la nueva matriz: ";
+                cin >> filas;
+                cout << endl;
+
+                while (filas < 1) {
+                    cout << "Ingrese un size valido: ";
+                    cin >> filas;
+                    cout << endl;
+                }
+
+                cout << "Ingrese las columnas de la matriz: ";
+                cin >> columnas;
+                cout << endl;
+
+                while (columnas < 1) {
+                    cout << "Ingrese un valor valido: ";
+                    cin >> columnas;
+                    cout << endl;
+                }
+
+                int** matriz = NULL;
+
+                //llamado a metodo generador de matrices manuales
+                matriz = genMatrizManual(filas, columnas);
+
+                matricesVector.push_back(matriz);
+                filasMatrices.push_back(filas);
+                columnasMatrices.push_back(columnas);
+
+                cout << "La matriz ha sido guardada exitosamente" << endl << endl;
+                
+                imprimirMatriz();
+                break;
+
+            }
+            case 2:
+            {
+
+                cout << "Ingreso automatioc de matriz" << endl << endl;
+
+                int filas = 0;
+                int columnas = 0;
+
+                cout << "Ingrese las filas de la nueva matriz: ";
+                cin >> filas;
+                cout << endl;
+
+                while (filas < 1) {
+                    cout << "Ingrese un size valido: ";
+                    cin >> filas;
+                    cout << endl;
+                }
+
+                cout << "Ingrese las columnas de la matriz: ";
+                cin >> columnas;
+                cout << endl;
+
+                while (columnas < 1) {
+                    cout << "Ingrese un valor valido: ";
+                    cin >> columnas;
+                    cout << endl;
+                }
+
+                int** matriz = NULL;
+
+                //llamado a metodo generador de matrices manuales
+                matriz = genMatrizAuto(filas, columnas);
+
+                matricesVector.push_back(matriz);
+                filasMatrices.push_back(filas);
+                columnasMatrices.push_back(columnas);
+
+                cout << "La matriz ha sido guardada exitosamente" << endl << endl;
+                
+                imprimirMatriz();
+
+                break;
+
+            }
+            case 3:
+            {
+
+                break;
+
+            }
+            case 4:
+            {
+                freeHeapVector();
+                cout << "Salio del programa... " << endl;
+
+                break;
+            }
+
+        }
+    }
+
 
     return 0;
 }
 
+//libera la memoria del vector de matrices
+void freeHeapVector(){
+    
+    for(int i = 0; i < matricesVector.size(); i++){
+        
+        int** &matriz = matricesVector.at(i);
+        
+        
+        for(int j = 0; j < filasMatrices.at(i); j++){
+            delete[] matriz[j];
+        }
+        
+        delete[] matriz;
+        
+        matriz = NULL;
+    }
+    
+    matricesVector.clear();
+}
+
+
+//genera una matriz aleatoria de tam n * m -50 a 50
+
+int** genMatrizAuto(int filas, int columnas) {
+
+    srand(time(0));
+
+    int** matriz = NULL;
+    matriz = new int* [filas];
+
+    for (int i = 0; i < filas; i++) {
+        matriz[i] = new int [columnas];
+    }
+
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+
+            matriz[i][j] = 1+(rand() % 50);
+        }
+    }
+
+    return matriz;
+
+}
+
+//genera una matriz llenada por el usuario
+
+int** genMatrizManual(int filas, int columnas) {
+
+    int** matriz = NULL;
+
+    matriz = new int* [filas];
+
+    for (int i = 0; i < filas; i++) {
+        matriz[i] = new int [columnas];
+    }
+
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+
+            cout << "Ingrese la posicion: " << i << ", " << j << endl;
+            cin >> matriz[i][j];
+        }
+    }
+
+    return matriz;
+}
+
+//imprime todas las matrices en el vector
+void imprimirMatriz(){
+    
+    for(int i = 0; i < matricesVector.size(); i++){
+        
+        int fila = filasMatrices.at(i);
+        int columna = columnasMatrices.at(i);
+        int** matriz = matricesVector.at(i);
+        
+        cout << "Matriz #" << i << endl;
+        
+        for(int j = 0; j < fila; j++){
+            for(int k = 0; k < fila; k++){
+                cout <<"[" << matriz[i][j] << "]\t"; 
+            }
+            cout << endl;
+        }
+        cout<< endl << endl;
+    }
+}
+
+//menu que valida el ingreso de la opcion del usuario
+
+int menu() {
+
+    int opcion = 0;
+
+    do {
+
+        cout << "       Menu        " << endl
+                << "1. Ingresar Matriz" << endl
+                << "2. Generar Matriz" << endl
+                << "3. Realizar Operaciones entre Matrices" << endl
+                << "4. Salir" << endl
+                << "Ingrese una opcion: ";
+
+        cin >> opcion;
+        cout << endl;
+
+        if (opcion > 4 || opcion < 1) {
+            cout << "La opcion ingresada no es valida intente de nuevo... \n\n";
+        }
+
+    } while (opcion > 4 || opcion < 1);
+
+    return opcion;
+}
---
+++ build/Debug/Cygwin-Windows/P3Lab5_DanielAlvarado.o.d
@@ -0,0 +1,2 @@
+build/Debug/Cygwin-Windows/P3Lab5_DanielAlvarado.o: \
+ P3Lab5_DanielAlvarado.cpp
